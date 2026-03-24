1bc321cf05638e8247ba5c67d1104fd7d48ebe4b
Magnus Lislevatn <primeid@users.noreply.github.com>
2026-03-24 15:27:21 +0100

    Retune about portrait pixel treatment

--- scripts/generate-about-portrait.sh
+++ scripts/generate-about-portrait.sh
@@ -15,18 +15,21 @@ magick "$input_path" \
   -gravity north \
   -crop 561x760+0+140 +repage \
   -colorspace Gray \
-  -contrast-stretch 4%x2% \
-  -sigmoidal-contrast 6,52% \
-  -resize 170x230! \
+  -contrast-stretch 3%x2% \
+  -sigmoidal-contrast 7,48% \
+  -resize 68x92! \
   -background '#050806' \
   -gravity center \
-  -extent 170x230 \
-  -fill '#88a39a' \
-  -colorize 14 \
-  -attenuate 0.08 +noise Multiplicative \
+  -extent 68x92 \
+  -ordered-dither o8x8,4 \
+  +dither \
+  -colors 18 \
+  -fill '#7f9a90' \
+  -colorize 10 \
+  -modulate 98,90,100 \
   -resize 340x460! \
   -filter point \
   -resize 680x920! \
-  -vignette 0x18+0+0 \
-  -quality 82 \
+  -vignette 0x12+0+0 \
+  -quality 80 \
   "$output_path"
